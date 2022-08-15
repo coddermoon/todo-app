9f0d4c2023e673edd9d3458d4bdfbc2a7b10d75b
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-15 14:03:50 +0600

    added a massage

--- app.js
+++ app.js
@@ -3,41 +3,61 @@
 const inputTodo = document.getElementById('todo')
 const submitTodo = document.getElementById('add-todo');
 const todoForm = document.getElementById('input-form')
-const ul = document.getElementById('lists')
+const ul = document.getElementById('lists');
+const massage = document.getElementById('massage')
 
 
+
+// show massage function
+
+const showMassage = (text, status) => {
+  massage.textContent = text
+  massage.classList.add(`bg-${status}`)
+  setTimeout(() => {
+    massage.textContent = ''
+    massage.classList.remove(`bg-${status}`)
+  }, 1000)
+}
 
 //add todudo
 
-const addTodo = (event)=>{
+const addTodo = (event) => {
 
-   event.preventDefault()
+  event.preventDefault()
   const info = inputTodo.value;
 
-//   genarate id 
+  //   genarate id 
 
-const randomId = Date.now().toString();
+  const randomId = Date.now().toString();
 
-// create function 
+  // create function 
 
-createTodo(randomId,info)
+  createTodo(randomId, info)
+
+  showMassage('successfully added', 'success')
+
+    
 
 }
 
 // create function
 
-const createTodo = (randomId,info)=>{
-  
-const li = document.createElement('li');
-li.id = randomId;
-li.className = 'list-item'
-li.innerHTML = `
+const createTodo = (randomId, info) => {
+
+  const li = document.createElement('li');
+  li.id = randomId;
+  li.className = 'list-item'
+  li.innerHTML = `
 
 <span>${info}</span>
 <span><button class="btn text-red" id="delete-btn">delete</button</span>
 
 `
-ul.appendChild(li)
+  ul.appendChild(li)
+
+
+
+
 
 
 }
@@ -46,5 +66,5 @@ ul.appendChild(li)
 
 //add event listener
 
-todoForm.addEventListener('submit',addTodo)
+todoForm.addEventListener('submit', addTodo)
 
